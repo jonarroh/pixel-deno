de22d45791fe04fa7cbc918a2075760cbbd3c893
jonarroh <jonarrodi99@gmail.com>
2023-05-01 13:01:46 -0600

    first commit

---
+++ shared/constants.ts
@@ -0,0 +1,14 @@
+export const WIDTH = 16;
+export const HEIGHT = 16;
+
+export const COLORS = [
+  "#FF0000",
+  "#00FF00",
+  "#0000FF",
+  "#FFFF00",
+  "#00FFFF",
+  "#FF00FF",
+  "#FFFFFF",
+  "#000000",
+  "#383838",
+];
